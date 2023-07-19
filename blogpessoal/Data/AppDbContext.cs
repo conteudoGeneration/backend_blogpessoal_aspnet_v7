f27f227314b14fe942db239b4f3b1fb0e01e8ddf
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-07-19 10:55:07 -0300

    Configurar Banco de dados

---
+++ blogpessoal/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blogpessoal.Data
+{
+    public class AppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
